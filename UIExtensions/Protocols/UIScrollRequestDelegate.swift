db4e0b44701933876c35f54ffb2bd0ea109cc390
Alexey Chekanov <chekanov@hotmail.com>
2018-03-07 17:21:14 -0500

    Size update and scrolling request notifications were added

---
+++ UIExtensions/Protocols/UIScrollRequestDelegate.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+public struct ScrollRequest {
+    
+    var rect: CGRect
+    var view: UIView
+    var animated: Bool
+}
+
+public protocol UIScrollRequestDelegate: class {
+    
+    var scrollRequest: Notification.Name { get }
+    var addresseeVerificationFunction: (Notification) -> Bool { get }
+    var scrollingFunction: (CGRect, UIView, Bool) -> () { get }
+    
+    func subscribeToScrollRequests()
+}
+
+
+public extension UIScrollRequestDelegate {
+    
+    func subscribeToScrollRequests() {
+        NotificationCenter.default.addObserver(forName: scrollRequest, object: nil, queue: nil) { [weak self] (notification) in
+            
+            guard let isAddressee = self?.addresseeVerificationFunction(notification) , isAddressee else { return }
+            
+            guard let data = notification.userInfo else { return }
+            
+            print(data)
+            
+           guard let request = data["request"] as? ScrollRequest else { return }
+            
+            print(request)
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(100), execute: { [weak self] in
+               
+                self?.scrollingFunction(request.rect, request.view, request.animated)
+                
+            })
+        }
+    }
+}
+
--- UIExtensions/UITextView/UITextView+sendScrollRequest.swift
+++ UIExtensions/UITextView/UITextView+sendScrollRequest.swift
@@ -13,7 +13,7 @@ public extension UITextView {
             // Create some space around
             caret = caret.offsetBy(dx: 0, dy: -caret.size.height/8)
             
-            let scrollRequest = ScrollRequest(view: self, rect: caret, animated: false)
+            let scrollRequest = ScrollRequest(rect: caret, view: self,  animated: false)
             
             var userInfo: [AnyHashable: Any] = [:]
             
@@ -33,11 +33,4 @@ public extension UITextView {
 public extension Notification.Name {
     
     static let scrollRequest = Notification.Name("UIViewScrollRequested")
-}
-
-public struct ScrollRequest {
-    
-    public var view: UIView
-    public var rect: CGRect
-    public var animated: Bool
 }
